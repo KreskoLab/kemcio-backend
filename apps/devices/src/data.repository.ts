03c5f51c72e2bfd3b5484538b50eb7a711942e0e
KreskoLab <redeezol@gmail.com>
2022-06-23 13:52:27 +0300

    feat (devices): add data repository

---
+++ apps/devices/src/data.repository.ts
@@ -0,0 +1,14 @@
+import { CreateDataDto } from '@app/common';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Data, DataDocument } from './schemas/data.schema';
+
+@Injectable()
+export class DataRepository {
+  constructor(@InjectModel(Data.name) private dataModel: Model<DataDocument>) {}
+
+  async save(newData: CreateDataDto): Promise<Data> {
+    return this.dataModel.create(newData);
+  }
+}
