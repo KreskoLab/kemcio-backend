a00a7dc6301360b5ae08f98d3398972f529eee83
KreskoLab <redeezol@gmail.com>
2022-07-04 15:32:37 +0300

    refactor (users): mongo filter code cleanup

--- apps/users/src/mongo-exception.filter.ts
+++ apps/users/src/mongo-exception.filter.ts
@@ -1,12 +1,10 @@
-import { Catch, ArgumentsHost, RpcExceptionFilter, ExceptionFilter } from '@nestjs/common';
+import { Catch, ExceptionFilter } from '@nestjs/common';
 import { MongoError } from 'mongodb';
-import { Response } from 'express';
-import { Observable, throwError } from 'rxjs';
-import { RpcException } from '@nestjs/microservices';
+import { throwError } from 'rxjs';
 
 @Catch(MongoError)
 export class MongoExceptionFilter implements ExceptionFilter {
-  catch(exception: MongoError, host: ArgumentsHost) {
+  catch(exception: MongoError) {
     switch (exception.code) {
       case 11000:
         const err = JSON.stringify({ code: 409, msg: 'User already exists' });
