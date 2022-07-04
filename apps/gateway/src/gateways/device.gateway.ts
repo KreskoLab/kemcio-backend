ab50f16594377b63924b2a01d4287f3f1a1cda71
KreskoLab <redeezol@gmail.com>
2022-07-04 22:52:47 +0300

    feat (gateway): add WebSocket gateway

---
+++ apps/gateway/src/gateways/device.gateway.ts
@@ -0,0 +1,31 @@
+import { Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { v4 as uuidv4 } from 'uuid';
+import { WebSocketGateway, MessageBody, SubscribeMessage } from '@nestjs/websockets';
+import { NewDeviceData, NewDeviceId } from '@app/common';
+import { firstValueFrom } from 'rxjs';
+
+@WebSocketGateway(8080, { path: '/device' })
+export class DevicesGateway {
+  constructor(@Inject('devices-new') private readonly devicesNewService: ClientProxy) {}
+
+  @SubscribeMessage('devices-new')
+  async handleEvent(
+    @MessageBody() data: NewDeviceData,
+  ): Promise<(NewDeviceId & Pick<NewDeviceData, 'clientId'>) | string> {
+    switch (data.status) {
+      case 'init':
+        const id = uuidv4();
+        const msg: NewDeviceData = { ...data, clientId: id };
+
+        const res = await firstValueFrom(this.devicesNewService.send<NewDeviceId, NewDeviceData>('init', msg));
+        return { ...res, clientId: id };
+
+      case 'flashed':
+        return firstValueFrom(this.devicesNewService.send<string, NewDeviceData>('flashed', data));
+
+      case 'aborted':
+        return firstValueFrom(this.devicesNewService.send<string, NewDeviceData>('aborted', data));
+    }
+  }
+}
