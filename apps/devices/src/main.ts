a479222bde380684e2e5484bb6cafca5aadc0c73
KreskoLab <redeezol@gmail.com>
2022-07-07 22:25:48 +0300

    feat (devices): get rabbitmq url from env

--- apps/devices/src/main.ts
+++ apps/devices/src/main.ts
@@ -1,10 +1,17 @@
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
 import { DevicesModule } from './devices.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const appContext = await NestFactory.createApplicationContext(DevicesModule);
+  const configService = appContext.get(ConfigService);
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(DevicesModule, {
     transport: Transport.RMQ,
+    options: {
+      urls: [configService.get<string>('RABITMQ_URL')],
+    },
   });
 
   app.listen();
