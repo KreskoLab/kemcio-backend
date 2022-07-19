ca28a7ca8724ae63d46a1465460d41d719b8e6f2
KreskoLab <redeezol@gmail.com>
2022-07-19 23:35:07 +0300

    feat (libs): add enums for CMD

---
+++ libs/common/src/enums/cmds/auth.ts
@@ -0,0 +1,5 @@
+export enum AUTH_CMD {
+  TOKENS = 'auth-tokens',
+  VERIFY_ACCESS_TOKEN = 'auth-verify-accessToken',
+  VERIFY_REFRESH_TOKEN = 'auth-verify-refreshToken',
+}
---
+++ libs/common/src/enums/cmds/user.ts
@@ -0,0 +1,8 @@
+export enum USERS_CMD {
+  DATA = 'users',
+  ADD = 'user-add',
+  UPDATE = 'user-update',
+  REMOVE = 'user-remove',
+  USER = 'user-data',
+  LOGIN = 'user-login',
+}
